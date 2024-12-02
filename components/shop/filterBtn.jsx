5e649ceaca10052f0220d769e6e574bcefcc3900
vicxtunespro <vicitug@gmail.com>
2024-12-02 06:16:18 -0800

    filter V2

--- components/shop/filterBtn.jsx
+++ components/shop/filterBtn.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState } from 'react'
+import { FaSearch } from "react-icons/fa";
 
 export default function FilterBtn() {
   const [filters, setFilters] = useState(false);
@@ -10,26 +11,31 @@ export default function FilterBtn() {
     setSearch(true);
   }
 
+  const handleFilter = () => {
+    setFilters(!filters)
+    setSearch(false)
+  }
+
   return (
     <div className='col-span-2 flex justify-center relative md:hidden'>
         <div 
         className='w-8 h-8 bg-rose-300 opacity-70 rounded-full flex justify-center items-center'
-        onClick = {()=>setFilters(true)}
+        onClick = {handleFilter}
         >
             <div className='w-6 h-6 bg-rose-500 rounded-full'></div>
         </div>
 
         {filters && (
           <div className='absolute w-screen -bottom-10 shadow flex justify-evenly items-center text-xs list-none'>
-            <li className='p-2'>LATEST PRODUCTS</li>
-            <li className='p-2'>TOP RATING</li>
-            <li className='p-2'>BEST SELLING</li>
-            <li className='p-2' onClick={handleSearch}>🔍</li>
+            <li onClick={() => setCategory('Latest')} className='cursor-pointer hover:underline'>LATEST PRODUCTS</li>
+            <li onClick={() => setCategory('Top Rating')} className='cursor-pointer hover:underline'>TOP RATING</li>
+            <li onClick={() => setCategory('Best Selling')} className='cursor-pointer hover:underline'>BEST SELLING</li>
+            <li className='p-2' onClick={handleSearch}><FaSearch /></li>
           </div>
         )}
         {search && (
-          <div className='absolute w-screen -bottom-10 shadow flex justify-evenly items-center text-xs list-none'>
-            <input type='text' placeholder='Product search' className='px-4 py-4 w-72 rounded-full'></input>
+          <div className='absolute w-screen -bottom-10  flex justify-evenly items-center text-xs list-none'>
+            <input type='text' placeholder='Product search' className='px-4 py-2 w-72 rounded-full'></input>
           </div>
         )}
     </div>
